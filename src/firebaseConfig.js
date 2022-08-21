b79a236e4caad346969c594d953749cad7507c74
Joshua Simon <joshy.d.simon@gmail.com>
2022-08-21 23:29:40 +1200

    final changes

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDajuKW16yqriJW-F0oE_r_A3xM29316ZI",
+  authDomain: "personal-website-dbafb.firebaseapp.com",
+  projectId: "personal-website-dbafb",
+  storageBucket: "personal-website-dbafb.appspot.com",
+  messagingSenderId: "988957863113",
+  appId: "1:988957863113:web:475855d109cb2affdaf118"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
